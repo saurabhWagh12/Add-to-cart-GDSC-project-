0dab8461ee27ca59d4f0c8a3f8bde0ff28fe8912
Saurabh Wagh <111182719+saurabhWagh12@users.noreply.github.com>
2023-06-20 20:47:36 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,34 +1,34 @@
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".close-modal");
-const btnsOpenModal = document.querySelector(".add_btn");
+// const modal = document.querySelector(".modal");
+// const overlay = document.querySelector(".overlay");
+// const btnCloseModal = document.querySelector(".close-modal");
+// const btnsOpenModal = document.querySelector(".add_btn");
 
 
 
-btnsOpenModal.addEventListener("click",openMODAL);
+// btnsOpenModal.addEventListener("click",openMODAL);
 
 
-btnCloseModal.addEventListener("click",closeMODAL);
-overlay.addEventListener("click",closeMODAL);
+// btnCloseModal.addEventListener("click",closeMODAL);
+// overlay.addEventListener("click",closeMODAL);
 
-function openMODAL(){
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-    }
+// function openMODAL(){
+//     modal.classList.remove("hidden");
+//     overlay.classList.remove("hidden");
+//     }
 
-function closeMODAL(){
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-}
+// function closeMODAL(){
+//     modal.classList.add("hidden");
+//     overlay.classList.add("hidden");
+// }
 
-document.addEventListener("keydown",function(event){
-    if(event.key==='Escape'){
-        // if(!modal.classList.contains("hidden"))
-        closeMODAL();
-    }
+// document.addEventListener("keydown",function(event){
+//     if(event.key==='Escape'){
+//         // if(!modal.classList.contains("hidden"))
+//         closeMODAL();
+//     }
 
-});
+// });
 
 
 
